47a188a6ce9ccb1a0d75b1f578b7db7d3983002f
snowbear1472 <snowbear1472@gmail.com>
2018-01-29 16:46:35 +0900

    reset

--- include/SnowPlan/PlanNode.hh
+++ include/SnowPlan/PlanNode.hh
@@ -1,22 +1,38 @@
 #pragma once
 
-#include <vector>
 #include <string>
-#include <ostream>
+#include <vector>
 
 
 namespace SnowPlan
 {
     class PlanNode
     {
-        public:
-            std::string name;
-            std::string text;
+        private:
+            std::string text_;
+            bool complete_;
 
-            std::vector<PlanNode> nodes;
+            std::vector<PlanNode> childs_;
 
         public:
-            void show(std::ostream& stream, const size_t& depth) const;
-            void show(const size_t& depth) const;
+            PlanNode() = default;
+            PlanNode(const std::string& _text, const bool& _complete, const std::vector<PlanNode>& _childs);
+
+            PlanNode(const PlanNode& copy) = delete;
+            PlanNode& operator = (const PlanNode& copy) = delete;
+
+            PlandNode(PlanNode&& move) = delete;
+
+        public:
+            const std::string& text() const;
+            const std::string& text(const std::string& _value);
+
+            const bool& complete() const;
+            const bool& complete(const bool& _value);
+
+            const std::vector& childs() const;
+            const std::vector& add_child(const PlanNode& _value);
+            const std::vector& remove_child(const PlanNode& _value);
+            const std::vector& find_child(const PlanNode& _value);
     };
 }
